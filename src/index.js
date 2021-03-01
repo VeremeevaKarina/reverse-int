ae791b53ba23e7d35a9051face02d9a6adda2250
karina.v.99933@gmail.com <karina.v.99933@gmail.com>
2021-03-01 22:15:37 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+   if (n < 0) {
+        n *= -1;
+    }
+
+    let digit, result = 0
+    while (n) {
+        digit = n % 10 
+        result = (result * 10) + digit 
+        n = n / 10 | 0
+    }  
+    return result
 }
